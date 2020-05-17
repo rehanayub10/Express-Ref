fc2f2de63eb5648fc890a4122f72128b471b84d9
Rehan Ayub <rehanayub20@gmail.com>
2020-05-16 23:17:05 -0400

    Added route params

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const port = 3000
 
 app.get('/', (req,res) => {
     res.send('Hello World!')
+});
+
+app.get('/example', (req,res) => {
+    res.send('Hitting example route');
 })
+
+//Using Route Parameters
+app.get('/example/:name/:age', (req,res) => {
+    console.log(req.params);
+    console.log(req.query);//follows the question mark in url, join with &
+    res.send(req.params.name + " : " + req.params.age);
+})
+
+//route params = MUST HAVE
+//query string param = optional
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
