6bb297a1a1b747859f4e95e16c54ddd531e7baab
Rehan Ayub <rehanayub20@gmail.com>
2020-05-17 00:52:05 -0400

    Added middleware

--- index2.js
+++ index2.js
@@ -10,19 +10,4 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
-app.get('/example', (req,res) => {
-    res.send('Hitting example route');
-})
-
-//Using Route Parameters
-app.get('/example/:name/:age', (req,res) => {
-    console.log(req.params);
-    console.log(req.query);//follows the question mark in url, join with &
-    res.send(req.params.name + " : " + req.params.age);
-})
-
-//route params = MUST HAVE
-//query string param = optional
-
-
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
